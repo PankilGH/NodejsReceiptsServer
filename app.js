17d65d7dfe7d7bb8c6e24a8df80488795159d903
pankilgh <pankil@Pankils-MacBook-Pro.local>
2015-05-03 12:52:25 -0400

    Can now query Panorama's web services directly, made yellow card yellow
    Can now query Panorama's web services directly - similar to curl.
    css change made yellow card yellow

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 
+
 var app = express();
 
 var port = process.env.PORT || 3000;
@@ -21,6 +22,8 @@ app.use(multer()); // for parsing multipart/form-data
 //for dynamic yellow card
 var request = require('request');
 var fs = require('fs');
+var https = require('https');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 
 app.get('/:id/yc', function(req, res){
@@ -33,16 +36,73 @@ request.get('http://libertyjavaopal2.mybluemix.net/rest/api/healthrecords/test/q
 				return console.log(err);
 			}
 			console.log("The file was saved!");
-		})
-	  }
-	})
-
-	res.writeHead(302, {
+			  
+			  	res.writeHead(302, {
   'Location': '/index.html'
   //add other headers here...
 })
 res	.end();
+			  
+		})
+	  }
+	})
 });
+
+
+app.get('/pan/:id/yc', function(req, res){
+console.log('Pan/id/yc - get - Hey I ran'+req.params.id);
+// Set the headers
+var headers = {
+    'dateTime':       '2015-05-30T09:30:10Z',
+    'network.identifier':     '192.168.0.1',
+	'Practitioner.organization.name': 'cSWO',
+	'Practitioner.name.text' : 'Dr. Smith',
+	'Practitioner.identifier.value' : '123456',
+	'Content-Type': 'application/x-www-form-urlencoded',
+}
+
+// Configure the request
+var options = {
+    url: 'https://76.75.162.34:9080/cswo-dstu2/cswo/immunization/query',
+    method: 'POST',
+    headers: headers,
+    form: {'Patient.name.family':'Tao', 'Patient.name.given':'Frank', 
+		   'Patient.identifier.value':req.params.id, 'Patient.birthdate':'2000-01-01', 
+		  'Patient.gender':'M', '_format': 'application/json'}
+}
+
+options.agent = new https.Agent(options);
+
+console.log("before response:")
+// Start the request
+request(options, function (error, response, body) {
+	console.log("response:"+ "error"+error)
+    if (!error && response.statusCode == 200) {
+        // Print out the response body
+        console.log(body)
+		if (!error && response.statusCode == 200) {
+		  fs.writeFile("./data4.json", body, function(err) {
+			if(err) {
+				return console.log(err);
+			}
+			console.log("The file was saved!");
+			
+			  res.writeHead(302, {
+			  'Location': '/index.html'
+			  //add other headers here...
+				})
+				res	.end();
+		})
+	  }
+    }
+	        console.log("body:"+body)
+
+})
+
+console.log("after response:")
+
+});
+
 
 app.post('/', function(req, res){
 	console.log('post - Hey I ran');
