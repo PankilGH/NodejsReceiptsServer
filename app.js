32aa74d28e21911de4b7498df6bd20ac020eb858
pankilgh <pankil@Pankils-MacBook-Pro.local>
2015-05-15 18:15:07 -0400

    Restructured Project
    removed dependency on data4.json
    redesigned to separate the search interface from IHR viewer from NodeJS
    server.
    Added fake Forecaster code in IHR viewer (yellowcard) uses something
    like FHIR immunizationRecommendation resource
    Added dynamic QR code in the centre of the IHR viewer
    Added demographics information of the client to the IHR viewer
    Added a time tracker code to track performance - can keep track of
    which database the data is from cloudant or panorama

--- app.js
+++ app.js
@@ -29,160 +29,20 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; //needed for Panorama certificat
 var Receipt = require('./modules/receipts.js');
 
 
-
-/**************************************************/
-/**************************************************/
-/**************************************************/
-
-
-//TODO: re-implement this to use Node.js cloudant's services
-
-/*
-//Yellow Card from Liberty Java's Cloudant
-app.get('/:id/yc', function(req, res){
-console.log('get - Hey I ran'+req.params.id);
-request.get('http://libertyjavaopal2.mybluemix.net/rest/api/healthrecords/test/query/hcn/fhir2/'+req.params.id, function (error, response, body) {
-	console.log(body);
-	  if (!error && response.statusCode == 200) {
-		  fs.writeFile("./data4.json", body, function(err) {
-			if(err) {
-				return console.log(err);
-			}
-			console.log("The file was saved!");
-			console.log(JSON.parse(body).status);	  
-			if (JSON.parse(body).status == "error"){
-				//search Panorama		
-				res.writeHead(302, {
-				'Location': '/error.html'
-				//add other headers here...
-				})
-				res.end();
-			}else {
-				
-				//res.writeHead(302, {
-				//'Location': '/yellowcard.html'
-				//add other headers here...
-				//})
-				
-				res.end();
-			} 
-		})
-	  }
-	})
-});
-*/
-
-
-
-
-
-//Yellow Card from Panorama
-app.get('/pan/:id/yc', function (req, res){
-	
-console.log('Pan/id/yc - get - Hey I ran. Looking for HCN: '+req.params.id);
-	// Set the headers
-var headers = {
-    'dateTime':       '2015-05-30T09:30:10Z',
-    'network.identifier':     '192.168.0.1',
-	'Practitioner.organization.name': 'cSWO',
-	'Practitioner.name.text' : 'Dr. Smith',
-	'Practitioner.identifier.value' : '123456',
-	'Content-Type': 'application/x-www-form-urlencoded',
-}
-
-// Configure the request
-var options = {
-    url: 'https://76.75.162.34:9080/cswo-dstu2/cswo/immunization/query',
-    method: 'POST',
-    headers: headers,
-    form: {'Patient.name.family':'Tao', 'Patient.name.given':'Frank', 
-		   'Patient.identifier.value':req.params.id, 'Patient.birthdate':'2000-01-01', 
-		  'Patient.gender':'M', '_format': 'application/json'}
-}
-
-options.agent = new https.Agent(options);
-// Start the request
-request(options, function (error, response, body) {
-	console.log("response:"+response.statusCode+ "error"+error)
-    if (!error && response.statusCode == 200) {
-        // Print out the response body
-        //console.log(body)
-		if (!error && response.statusCode == 200) {
-		  fs.writeFile("./data4.json", body, function(err) {
-			if(err) {
-				return console.log(err);
-			}
-			console.log("The file was saved!");
-			
-			// HCN is used as document ID and to search for documents
-			// cloudant cache service
-			// assume Panorama contains the most up to date document
-			// always replace cloudant copy with the Panorama copy
-			db = cloudant.use(dbNames.dbihrfhir2);
-			var docSave = JSON.parse(body);
-			
-			// get document records _rev if it exists
-			db.get(req.params.id, {revs_info: true}, function(error, docbody){
-				if (!error){
-					docSave._rev = docbody._rev;
-					console.log(docSave._rev);
-				}else{
-					//console.log(error);
-				}
-				
-				// insert new document or update existing one
-				db.insert(docSave, req.params.id, function(err, body) {
-				if (!err){
-					console.log(body)
-				}
-				else{
-					console.log(err)
-				}
-				})
-				
-			});
-	  
-			  
-			//console.log(JSON.parse(body).status);	  
-			if (JSON.parse(body).status == "error"){
-				//search error	-- this probably never happens
-				res.json({status:"error"});
-				res.end();
-			}else {
-				/*
-				res.writeHead(302, {
-				'Location': '/yellowcard.html'
-				//add other headers here...
-				})
-				*/
-				res.json({status:"success"});
-				res.end();
-			} 
-		})
-	  }
-    }
-	else{
-	// body = There is no immunization records for the Patient
-	
-	/*
-	res.writeHead(302, {
-	'Location': '/error.html'
-	//add other headers here...
-	})
-	*/
-	res.json({status:"error"});
-	res.end();
-	}
-	//console.log("body:"+body);
-})
+//CORS
+//http://enable-cors.org/server.html
+// This is not safe - change the implementation
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
+/**************************************************/
+/** Connecting to Cloudant ************************/
+/**************************************************/
 
-/**************************************************/
-/**************************************************/
-/**************************************************/
-
-//Connecting to Cloudant
+//Setup for connecting to Cloudant DBs
 var db;
 var cloudant;
 var dbNames = {
@@ -202,6 +62,11 @@ var dbCredentials = {
 	dbihrfhir2: 'ihrfhir2'
 };
 
+
+/**
+* Cloudant database connection setup
+* This needs some clean up and environment variable set up for local testing
+*/
 function initDBConnection() {
 	
 	if(process.env.VCAP_SERVICES) {
@@ -270,9 +135,16 @@ function initDBConnection() {
 
 initDBConnection();
 
+/**************************************************/
+/**************************************************/
+/**************************************************/
 
-//http://irfhir.mybluemix.net/rest/fhir/receipt/
-//localhost:8000/rest/fhir/receipt
+
+/**
+* Used to store receipts from QR-EMR
+* http://irfhir.mybluemix.net/rest/fhir/receipt/
+* localhost:8000/rest/fhir/receipt
+*/
 app.post('/rest/fhir/receipt', function(req, res){
 	console.log('post - Hey I ran');
 	//console.log(req.body);
@@ -319,23 +191,22 @@ app.post('/rest/fhir/receipt', function(req, res){
 });
 
 
-app.get('/:id/yc', function(req, res){
-	console.log('get - Hey I ran'+req.params.id);
-	db = cloudant.use(dbNames.dbihrfhir2);
-	// get document records _rev if it exists
-	db.get(req.params.id, {revs_info: true}, function(error, doc){
+/**************************************************/
+/** IHR Server side cache and search **************/
+/**************************************************/
+
+/**
+* Search cloudant (cache) only
+*/
+app.get('/cache/:id/yc', function(req, res){
+	console.log('Checking cloudant cache only for hcn: '+req.params.id);
+	var db = cloudant.use(dbNames.dbihrfhir2);
+	db.get(req.params.id, {revs_info: false}, function(error, doc){
 		if (!error){
-			fs.writeFile("./data4.json", JSON.stringify(doc), function(err) {
-			if(err) {
-				res.json({status:"error"});
-				return console.log(err);
-				res.end();
-			}else{
-				console.log("The file was saved!"); 
-				res.json({status:"success"});
-				res.end();
-			}
-			});
+			doc.status = "success";
+			doc.dataFrom = "Cloudant";
+			res.json(doc);
+			res.end();
 		}else{
 			//console.log(error);
 			res.json({status:"error"});
@@ -344,7 +215,232 @@ app.get('/:id/yc', function(req, res){
 	});
 });
 
+/**
+* Search cloudant (cache) if not found check Panorama
+*/
+app.get('/:id/yc', function(req, res){
+	console.log('Checking cloudant cache first if not found in cache then Panorama for hcn: '+req.params.id);
+	var db = cloudant.use(dbNames.dbihrfhir2);
+	// check the cache first
+	db.get(req.params.id, {revs_info: false}, function(error, doc){
+		console.log("checking cloudant");
+		if (!error){
+			console.log("found in cloudant");
+			doc.status = "success";
+			doc.dataFrom = "Cloudant";
+			res.json(doc);
+			res.end();
+		}else{
+			// cache search failed now check Panorama
+			
+			
+			/***Most of this is duplicate code - refactor and fix***/
+			
+			
+							// Set the headers
+							var headers = {
+								'dateTime':       '2015-05-30T09:30:10Z',
+								'network.identifier':     '192.168.0.1',
+								'Practitioner.organization.name': 'cSWO',
+								'Practitioner.name.text' : 'Dr. Smith',
+								'Practitioner.identifier.value' : '123456',
+								'Content-Type': 'application/x-www-form-urlencoded',
+							}
 
+							// Configure the request
+							var options = {
+								url: 'https://76.75.162.34:9080/cswo-dstu2/cswo/immunization/query',
+								method: 'POST',
+								headers: headers,
+								form: {'Patient.name.family':'Tao', 'Patient.name.given':'Frank', 
+									   'Patient.identifier.value':req.params.id, 'Patient.birthdate':'2000-01-01', 
+									  'Patient.gender':'M', '_format': 'application/json'}
+							}
+
+							options.agent = new https.Agent(options);
+							// Start the request
+							request(options, function (error, response, body) {
+								
+								console.log("cannot find in Cloudant checking Panorama");
+
+								
+								console.log("response:"+response.statusCode+ "error"+error)
+								if (!error && response.statusCode == 200) {
+
+										console.log("found in Panorama");
+
+										// HCN is used as document ID and to search for documents
+										// cloudant cache service
+										// assume Panorama contains the most up to date document
+										// always replace cloudant copy with the Panorama copy
+										db = cloudant.use(dbNames.dbihrfhir2);
+										var docSave = JSON.parse(body);
+
+										// get document records _rev if it exists
+										db.get(req.params.id, {revs_info: false}, function(error, docbody){
+											if (!error){
+												docSave._rev = docbody._rev;
+												console.log(docSave._rev);
+											}else{
+												//console.log(error);
+											}
+
+											// insert new document or update existing one
+											db.insert(docSave, req.params.id, function(err, body) {
+											if (!err){
+												console.log("updated Cloudant successfully");
+
+												//console.log(body)
+											}
+											else{
+												console.log("Cloudant update failed");
+
+												
+												console.log(err)
+											}
+											})
+
+										});
+
+										if (JSON.parse(body).status == "error"){
+											//search error	-- this probably never happens
+											res.json({status:"error"});
+											res.end();
+										}else {
+											docSave.status = "success";
+											docSave.dataFrom = "Panorama"
+											res.json(docSave);
+											res.end();
+										} 
+								}
+								else{
+								// body = There is no immunization records for the Patient
+									console.log("cannot find in Cloudant or Panorama");
+
+									res.json({status:"error"});
+									res.end();
+								}
+							})
+
+		}
+	});
+});
+
+/**
+* Search Panorama only
+*/
+app.get('/panorama/:id/yc', function (req, res){
+	
+console.log('panorama/id/yc - Looking in Panorama only for HCN: '+req.params.id);
+	// Set the headers
+var headers = {
+    'dateTime':       '2015-05-30T09:30:10Z',
+    'network.identifier':     '192.168.0.1',
+	'Practitioner.organization.name': 'cSWO',
+	'Practitioner.name.text' : 'Dr. Smith',
+	'Practitioner.identifier.value' : '123456',
+	'Content-Type': 'application/x-www-form-urlencoded',
+}
+
+// Configure the request
+var options = {
+    url: 'https://76.75.162.34:9080/cswo-dstu2/cswo/immunization/query',
+    method: 'POST',
+    headers: headers,
+    form: {'Patient.name.family':'Tao', 'Patient.name.given':'Frank', 
+		   'Patient.identifier.value':req.params.id, 'Patient.birthdate':'2000-01-01', 
+		  'Patient.gender':'M', '_format': 'application/json'}
+}
+
+options.agent = new https.Agent(options);
+// Start the request
+request(options, function (error, response, body) {
+	console.log("response:"+response.statusCode+ "error"+error)
+    if (!error && response.statusCode == 200) {
+        
+			// HCN is used as document ID and to search for documents
+			// cloudant cache service
+			// assume Panorama contains the most up to date document
+			// always replace cloudant copy with the Panorama copy
+			db = cloudant.use(dbNames.dbihrfhir2);
+			var docSave = JSON.parse(body);
+			
+			// get document records _rev if it exists
+			db.get(req.params.id, {revs_info: false}, function(error, docbody){
+				if (!error){
+					docSave._rev = docbody._rev;
+					console.log(docSave._rev);
+				}else{
+					//console.log(error);
+				}
+				
+				// insert new document or update existing one
+				db.insert(docSave, req.params.id, function(err, body) {
+				if (!err){
+					console.log(body)
+				}
+				else{
+					console.log(err)
+				}
+				})
+				
+			});
+	  
+			//console.log(JSON.parse(body).status);	  
+			if (JSON.parse(body).status == "error"){
+				//search error	-- this probably never happens
+				res.json({status:"error"});
+				res.end();
+			}else {
+				docSave.status = "success";
+				docSave.dataFrom = "Panorama";
+				res.json(docSave);
+				res.end();
+			} 
+    }
+	else{
+	// body = There is no immunization records for the Patient
+		res.json({status:"error"});
+		res.end();
+	}
+})
+});
+
+
+/**
+* Clean up db docs
+*/
+app.get('/cache/clean', function(req, res){
+	console.log('Remove most documents from cloudant keep specific ones only');
+	var db = cloudant.use(dbNames.dbihrfhir2);
+	var result = {status:"error"};
+	var count = 0;
+	var idsToKeep = ["6025157170", "7703432802", "6337565391", "2484584624"];
+	db.list(function(err, body) {
+  		if (!err) {
+			body.rows.forEach(function(doc) {
+				//if this document id is not in the list of ids to keep then delete
+			if (idsToKeep.indexOf(doc.id) == -1){
+				//delete
+				db.destroy(doc.id, doc.value.rev, function(err, body) {
+						if (!err){
+							count += 1;
+							console.log(body)
+						}
+					})
+				}
+			})
+			result = {status:"success", deleted:count};
+			res.json(result);
+			res.end();
+		}
+		else {
+			console.log(err);
+			res.json(result);
+			res.end();
+		}
+	})
+});
 
 app.listen(port, function(){
     console.log('Gulp is running my app on PORT: ' + port);
